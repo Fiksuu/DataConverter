4914c249e9f81fbe02b2dd804aa4a63fa4db8549
Piotr Fiks <piotrfiks560@gmail.com>
2024-07-13 00:21:41 +0200

    Added data conversion between JSON, XML, and YAML

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import json
+import xml.etree.ElementTree as ET
+import yaml
 
 def parse_arguments():
     if len(sys.argv) != 3:
@@ -27,11 +29,62 @@ def save_json(data, file_path):
         print(f"Failed to save JSON file: {e}")
         sys.exit(1)
 
+def load_xml(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        return root
+    except Exception as e:
+        print(f"Failed to load XML file: {e}")
+        sys.exit(1)
+
+def save_xml(data, file_path):
+    try:
+        tree = ET.ElementTree(data)
+        tree.write(file_path)
+    except Exception as e:
+        print(f"Failed to save XML file: {e}")
+        sys.exit(1)
+
+def load_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+        return data
+    except Exception as e:
+        print(f"Failed to load YAML file: {e}")
+        sys.exit(1)
+
+def save_yaml(data, file_path):
+    try:
+        with open(file_path, 'w') as file:
+            yaml.safe_dump(data, file)
+    except Exception as e:
+        print(f"Failed to save YAML file: {e}")
+        sys.exit(1)
+
+
 
 if __name__ == "__main__":
     input_path, output_path = parse_arguments()
     if input_path.endswith('.json'):
         data = load_json(input_path)
+    elif input_path.endswith('.xml'):
+        data = load_xml(input_path)
+    elif input_path.endswith('.yaml') or input_path.endswith('.yml'):
+        data = load_yaml(input_path)
+    else:
+        print("Unsupported input format")
+        sys.exit(1)
+
     if output_path.endswith('.json'):
         save_json(data, output_path)
-        print(f"Data saved to {output_path}")
+    elif output_path.endswith('.xml'):
+        save_xml(data, output_path)
+    elif output_path.endswith('.yaml') or output_path.endswith('.yml'):
+        save_yaml(data, output_path)
+    else:
+        print("Unsupported output format")
+        sys.exit(1)
+
+    print(f"Data converted and saved to {output_path}")
